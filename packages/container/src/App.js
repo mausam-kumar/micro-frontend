2b76fde670fa23193a3042b737923f85952fa626
mausam-kumar <mausam_oct@outlook.com>
2024-08-31 22:40:31 +0530

    Update text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./component/MarketingApp"
 const App = () => {
     return <div>
-        <h1>Container App</h1>
+        <h1>Container App Shell</h1>
         <hr />
         <MarketingApp />
     </div>
