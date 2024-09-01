b3ca1599cfd91fea13fc680b4240023a7a3ad2b9
mausam-kumar <mausam_oct@outlook.com>
2024-09-01 23:30:11 +0530

    Fix container and sub-app navigation

--- packages/container/src/component/MarketingApp.js
+++ packages/container/src/component/MarketingApp.js
@@ -1,12 +1,24 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
     const ref = useRef(null)
+    const history = useHistory()
 
     useEffect(() => {
-        mount(ref.current)
-    }, [ref.current])
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname)
+                }
+            }
+        })
+        if (onParentNavigate) {
+            history.listen(onParentNavigate)
+        }
+    }, [])
 
     return <div ref={ref} />
 };
